6a8ed920adf2eac2b0109a7bf66b69f7844cfc68
Michael Pilosov <40366263+mathematicalmichael@users.noreply.github.com>
2020-07-23 10:55:10 -0600

    Update and rename test_skeleton.py to test_fun.py
    still not going to work

--- tests/test_fun.py
+++ tests/test_fun.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 
 import pytest
-from mud.skeleton import fib
+import mud.fun as mdf
 
 __author__ = "Mathematical Michael"
 __copyright__ = "Mathematical Michael"
 __license__ = "mit"
 
 
-def test_fib():
+def test_fun():
     assert fib(1) == 1
     assert fib(2) == 1
     assert fib(7) == 13
