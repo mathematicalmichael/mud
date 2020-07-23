609fe2dda9cd2f514842b5eb3451c5de68529c96
Mathematical Michael <consistentbayes@gmail.com>
2020-07-23 14:54:35 +0000

    get rid of scipy

--- src/mud/plotting.py
+++ src/mud/plotting.py
@@ -1,6 +1,66 @@
 from matplotlib import pyplot as plt
 import numpy as np
 
+def null_space(A, rcond=None):
+    """
+    Construct an orthonormal basis for the null space of A using SVD
+
+    Parameters
+    ----------
+    A : (M, N) array_like
+        Input array
+    rcond : float, optional
+        Relative condition number. Singular values ``s`` smaller than
+        ``rcond * max(s)`` are considered zero.
+        Default: floating point eps * max(M,N).
+
+    Returns
+    -------
+    Z : (N, K) ndarray
+        Orthonormal basis for the null space of A.
+        K = dimension of effective null space, as determined by rcond
+
+    See also
+    --------
+    svd : Singular value decomposition of a matrix
+    orth : Matrix range
+
+    Examples
+    --------
+    One-dimensional null space:
+
+    >>> from scipy.linalg import null_space
+    >>> A = np.array([[1, 1], [1, 1]])
+    >>> ns = null_space(A)
+    >>> ns * np.sign(ns[0,0])  # Remove the sign ambiguity of the vector
+    array([[ 0.70710678],
+           [-0.70710678]])
+
+    Two-dimensional null space:
+
+    >>> B = np.random.rand(3, 5)
+    >>> Z = null_space(B)
+    >>> Z.shape
+    (5, 2)
+    >>> np.allclose(B.dot(Z), 0)
+    True
+
+    The basis vectors are orthonormal (up to rounding error):
+
+    >>> Z.T.dot(Z)
+    array([[  1.00000000e+00,   6.92087741e-17],
+           [  6.92087741e-17,   1.00000000e+00]])
+
+    """
+    u, s, vh = np.linalg.svd(A, full_matrices=True)
+    M, N = u.shape[0], vh.shape[1]
+    if rcond is None:
+        rcond = np.finfo(s.dtype).eps * max(M, N)
+    tol = np.amax(s) * rcond
+    num = np.sum(s > tol, dtype=int)
+    Q = vh[num:,:].T.conj()
+    return Q
+
 def plotChain(mud_chain, ref_param, color='k'):
     num_steps = len(mud_chain)
     current_point = mud_chain[0]
@@ -19,7 +79,7 @@ def plotChain(mud_chain, ref_param, color='k'):
 
 def plot_contours(A,color='k'):
     numQoI = A.shape[0]
-    AA = np.hstack([sp.linalg.null_space(A[i,:].reshape(1,-1)) for i in range(numQoI)]).T
+    AA = np.hstack([null_space(A[i,:].reshape(1,-1)) for i in range(numQoI)]).T
     for i in range(numQoI):
         plt.plot([0.5-AA[i,0],0.5+AA[i,0]], [0.5-AA[i,1], 0.5+AA[i,1]], c=color, ls=':')
 
