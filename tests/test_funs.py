31bd9e02960745cdae5a3bd35ab206fa7d16da49
Michael Pilosov <consistentbayes@gmail.com>
2020-11-01 02:57:02 +0000

    adding sensible defaults to mud fun + a basic test

--- tests/test_funs.py
+++ tests/test_funs.py
@@ -18,3 +18,17 @@ class TestIdentityInitialCovariance(unittest.TestCase):
     def test_that_R_inverse_is_zero(self):
         assert np.linalg.norm(mdf.makeRi(self.A, self.C)) < 1E-14
 
+    def test_solutions_with_orthogonal_map(self):
+        # Arrange
+        t = np.random.randn(2,1)
+        A = np.random.randn(2,2)
+        b = np.random.randn(2,1)
+        # m = np.zeros(2,1)
+        c = self.C
+
+        # Act
+        y = A@t + b
+        sol = mdf.mud_sol(A, b, y, cov=c)
+        err = sol - t
+        # Assert
+        assert np.linalg.norm(err) < 1E-14
