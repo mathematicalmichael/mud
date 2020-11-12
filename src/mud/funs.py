20dfd88e3f84e0edee5b292e01331ed816d0e664
Michael Pilosov <consistentbayes@gmail.com>
2020-11-11 21:50:42 -0700

    new wrapper functions

--- src/mud/funs.py
+++ src/mud/funs.py
@@ -134,18 +134,25 @@ def check_args(A, b, y, mean, cov, data_cov):
 
     return ravel, z, mean, cov, data_cov
 
+def mud_sol(A, b, y=None, mean=None, cov=None, data_cov=None, return_pred=False):
     """
-    Doesn't use R directly, uses new equations.
-    This presents the equation as a rank-k update
-    to the error of the initial estimate.
+    For SWE problem, we are inverting N(0,1).
+    This is the default value for `data_cov`.
     """
-    if data_cov is None: data_cov = np.eye(A.shape[0])
-    if cov is None: cov = np.eye(A.shape[1])
-    if mean is None: mean = np.zeros((A.shape[1],1))
-    if y is None: y = np.zeros((A.shape[0],1))
+    ravel, z, mean, cov, _ = check_args(A, b, y, mean, cov, data_cov)
+    inv_pred_cov = np.linalg.pinv(A@cov@A.T)
+    update = cov@A.T@inv_pred_cov
+    mud_point = mean + update@z
 
-    z = y.ravel() - b.ravel() - (A@mean).ravel()
-    z = z.reshape(-1,1)
+    if ravel:
+        # When y was passed as a 1d-array, we flatten the coefficients.
+        mud_point = mud_point.ravel()
+
+    if return_pred:
+        return mud_point, update
+    else:
+        return mud_point
+
 
 def updated_cov(X, init_cov, data_cov):
     pred_cov = X@init_cov@A.T
@@ -160,21 +167,41 @@ def updated_cov(X, init_cov, data_cov):
 
     return up_cov
 
+def mud_sol_alt(A, b, y=None, mean=None, cov=None, data_cov=None, return_pred=False):
+    """
+    Doesn't use R directly, uses new equations.
+    This presents the equation as a rank-k update
+    to the error of the initial estimate.
+    """
+    ravel, z, mean, cov, data_cov = check_args(A, b, y, mean, cov, data_cov)
+    up_cov = updated_cov(X=A, init_cov=cov, data_cov=data_cov)
+    update = up_cov @ A.T @ np.linalg.inv(data_cov)
+    mud_point = mean + update@z
+
+    if ravel:
+        # When y was passed as a 1d-array, we flatten the coefficients.
+        mud_point = mud_point.ravel()
+
+    if return_pred:
+        return mud_point, update
+    else:
+        return mud_point
 
 
-def map_sol(A, b, y=None, mean=None, cov=None, data_cov=None, w=1):
-    if data_cov is None: data_cov = np.eye(A.shape[0])
-    if cov is None: cov = np.eye(A.shape[1])
-    if mean is None: mean = np.zeros((A.shape[1],1))
-    if y is None: y = np.zeros((A.shape[0],1))
-
-    z = y.ravel() - b.ravel() - (A@mean).ravel()
-    z = z.reshape(-1,1)
-
-    precision = np.linalg.inv(A.T@np.linalg.inv(data_cov)@A + w*np.linalg.inv(cov))
-    update = precision@A.T@np.linalg.inv(data_cov)
+def map_sol(A, b, y=None, mean=None, cov=None, data_cov=None, w=1, return_pred=False):
+    ravel, z, mean, cov, data_cov = check_args(A, b, y, mean, cov, data_cov)
+    post_cov = np.linalg.inv(A.T@np.linalg.inv(data_cov)@A + w*np.linalg.inv(cov))
+    update = post_cov@A.T@np.linalg.inv(data_cov)
     map_point = mean.ravel() + (update @ z).ravel()
-    return map_point.reshape(-1,1)
+
+    if ravel:
+        # When y was passed as a 1d-array, we flatten the coefficients.
+        map_point = map_point.ravel()
+
+    if return_pred:
+        return map_point, update
+    else:
+        return map_point
 
 
 def performEpoch(A, b, y, initial_mean, initial_cov, data_cov=None, idx=None):
