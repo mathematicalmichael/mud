73990c53dde780b48974fffb90906bec6d837cfa
Michael Pilosov <consistentbayes@gmail.com>
2021-01-17 02:08:58 +0000

    migrate test

--- tests/test_util.py
+++ tests/test_util.py
@@ -2,7 +2,6 @@
 
 import unittest
 import mud.util as mdu
-import numpy as np
 
 __author__ = "Mathematical Michael"
 __copyright__ = "Mathematical Michael"
@@ -38,19 +37,3 @@ class TestUtil(unittest.TestCase):
         # Assert
         for i, tol in enumerate(tolerances):
             assert 3 * result[i] < tol
-
-
-class TestRandomSetups(unittest.TestCase):
-    def test_random_map(self):
-        for d in ['normal', 'uniform', None]:
-            for r in [True, False]:
-                for _dim_in in [1, 5, 10]:
-                    A = mdu.createRandomLinearMap(dim_input=_dim_in,
-                                                  dim_output=10,
-                                                  dist=d,
-                                                  repeated=r)
-                    assert A is not None
-                    if not r:
-                        assert np.linalg.matrix_rank(A) == _dim_in
-                    else:
-                        assert np.linalg.matrix_rank(A) == 1
