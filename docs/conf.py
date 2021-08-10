bf09b778fdd1d01f6ab3ea153465586b68b376db
Michael Pilosov <consistentbayes@gmail.com>
2021-08-10 01:50:25 +0000

    logo

--- docs/conf.py
+++ docs/conf.py
@@ -189,7 +189,7 @@ else:
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-# html_logo = ""
+html_logo = "logo.png"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
@@ -270,7 +270,7 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-# latex_logo = ""
+latex_logo = "logo.png"
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
