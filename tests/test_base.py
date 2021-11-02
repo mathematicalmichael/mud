048d898392778471911fa353f4f6ec5966c769b9
Michael Pilosov <consistentbayes@gmail.com>
2021-11-02 13:40:32 +0000

    add test for setting weights twice

--- tests/test_base.py
+++ tests/test_base.py
@@ -22,6 +22,26 @@ def test_identity_mud_problem_1D(identity_problem_mud_1D):
     assert np.mean(ratio) > 0
 
 
+def test_we_can_set_weights_in_predicted(identity_problem_mud_1D_equal_weights):
+    """Mimicks existing usage in mud-examples"""
+    # Arrange
+    # weights were used for initialization
+    D = identity_problem_mud_1D_equal_weights
+    D.set_initial()  # domain has been set -> uniform as default
+    # want to make sure we can set weights on predicted
+    D.set_predicted(weights=D._in)
+
+    # Act
+    mud_point = D.estimate()
+    updated_density = D._up
+    ratio = D._r
+
+    # Assert
+    assert np.round(mud_point, 1) == 0.5
+    assert np.sum(updated_density) > 0
+    assert np.mean(ratio) > 0
+
+
 def test_identity_mud_1D_with_equal_weights(identity_problem_mud_1D_equal_weights):
     # Arrange
     D = identity_problem_mud_1D_equal_weights
