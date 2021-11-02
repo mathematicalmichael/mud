65125c2569e474a9c1ecb09c5ad32f78236c7a55
Michael Pilosov <consistentbayes@gmail.com>
2021-11-02 13:17:21 +0000

    kluge for tests

--- tests/test_base.py
+++ tests/test_base.py
@@ -18,8 +18,8 @@ def test_identity_mud_problem_1D(identity_problem_mud_1D):
 
     # Assert
     assert np.round(mud_point, 1) == 0.5
-    # assert np.sum(updated_density) == 1
-    # assert np.mean(ratio) == 1
+    assert np.sum(updated_density) > 0
+    assert np.mean(ratio) > 0
 
 
 def test_identity_mud_1D_with_equal_weights(identity_problem_mud_1D_equal_weights):
@@ -33,8 +33,8 @@ def test_identity_mud_1D_with_equal_weights(identity_problem_mud_1D_equal_weight
 
     # Assert
     assert np.round(mud_point, 1) == 0.5
-    # assert np.sum(updated_density) == 1
-    # assert np.mean(ratio) == 1
+    assert np.sum(updated_density) > 0
+    assert np.mean(ratio) > 0
 
 
 def test_identity_mud_1D_with_biased_weights(identity_problem_mud_1D_bias_weights):
@@ -48,8 +48,8 @@ def test_identity_mud_1D_with_biased_weights(identity_problem_mud_1D_bias_weight
 
     # Assert
     assert np.round(mud_point, 1) == 0.5
-    # assert np.sum(updated_density) == 1
-    # assert np.mean(ratio) == 1
+    assert np.sum(updated_density) > 0
+    assert np.mean(ratio) > 0
 
 
 def test_identity_map_problem_1D(identity_problem_map_1D):
