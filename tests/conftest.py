08e926375879f2fc6d39febed526148b645e9c9a
Michael Pilosov <consistentbayes@gmail.com>
2021-11-13 19:40:29 +0000

    better reproducibility

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,7 +16,14 @@ import numpy as np
 
 @pytest.fixture
 def problem_generator_identity_1D():
-    def identity_uniform_1D(num_samples, num_obs, y_true=0.5, noise=0.05, weights=None):
+    def identity_uniform_1D(
+        num_samples=5000, num_obs=10, y_true=0.5, noise=0.05, weights=None
+    ):
+        """
+        Sets up an inverse problem using the unit domain and uniform distribution
+        under an identity map. This is equivalent to studying a \"steady state\" signal over time,
+        or taking repeated measurements of the same quantity to reduce variance in the uncertainty.
+        """
         dist = ds.uniform(loc=0, scale=1)
         X = dist.rvs(size=(num_samples, 1))
         y_pred = np.repeat(X, num_obs, 1)
@@ -38,10 +45,7 @@ def problem_generator_identity_1D():
 
 @pytest.fixture
 def identity_problem_mud_1D(problem_generator_identity_1D):
-    return problem_generator_identity_1D(
-        num_samples=1000,
-        num_obs=10,
-    )
+    return problem_generator_identity_1D()
 
 
 @pytest.fixture
