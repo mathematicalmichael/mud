6d55b347ff0466c4b474a699ab02e02da77ab5f0
Michael Pilosov <consistentbayes@gmail.com>
2022-07-04 16:43:48 -0600

    fix flake8

--- src/mud/base.py
+++ src/mud/base.py
@@ -1,4 +1,4 @@
-from typing import Callable, List, Union
+from typing import Callable, List, Optional, Union
 
 import numpy as np
 
--- src/mud/util.py
+++ src/mud/util.py
@@ -1,4 +1,4 @@
-from typing import List, Optional, Tuple, Union
+from typing import List, Tuple, Union
 
 import numpy as np
 from numpy.typing import ArrayLike
